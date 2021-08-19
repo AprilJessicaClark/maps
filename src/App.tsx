c21d7dad9e19c15304587826483970a7c0ccc535
April <apriljessicaclark@gmail.com>
2021-08-19 16:33:08 -0400

    Created form and map

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,155 @@
+import { LatLngExpression } from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import * as _ from 'lodash';
 import React, { createRef } from 'react';
-import { PDFDownloadLink, Image, Document, Page, Text, View, StyleSheet, pdf } from '@react-pdf/renderer';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { LatLngExpression, DivIcon, Map, icon, Marker as LMarker } from 'leaflet';
-import { saveAs } from 'file-saver';
-import "leaflet/dist/leaflet.css"
-import iconImage from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
-const LeafletImage = require("leaflet-image");
-const DefaultIcon = icon({
-  iconUrl: iconImage,
-  shadowUrl: iconShadow,
-  iconSize: [20, 30],
-  iconAnchor: [0, 0]
-});
-
-export default class App extends React.Component<{}, { src: string }> {
-
-  constructor(props: {} | Readonly<{}>) {
-    super(props);
-    LMarker.prototype.options.icon = DefaultIcon;
-    this.generatePdf = this.generatePdf.bind(this);
-  }
-
-  map?: Map;
-
-  // Create styles
-  styles = StyleSheet.create({
-    page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4'
-    },
-    section: {
-      border: '1px solid #000',
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
-    }
-  });
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import 'reflect-metadata';
+import LocationMap, { mapKey } from './LocationMap';
+import { ObjectForm } from './ObjectForm';
+import { extraSpace, options, placeHolder, validate } from './ValidationDecorator';
 
 
+type calculated = ((o: FormSchema) => string)
 
-
-
-  position: LatLngExpression = [51.505, -0.09];
-  myMap() {
-    return (
-      <MapContainer whenCreated={map => { this.map = map }} style={{ height: "512px", width: "512px" }} center={this.position} zoom={13}>
-        <Marker icon={DefaultIcon} position={this.position}></Marker>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    )
-  }
-
-
-  async generatePdf(mapInstance?: Map) {
-    return await 
-    new Promise((resolve, reject) => (LeafletImage(mapInstance, async (_err: any, canvas: { toDataURL: () => string; }) => {
-      let src = canvas.toDataURL();
-      resolve(pdf(
-        <Document>
-          <Page size="A4" style={this.styles.page}>
-            <View style={this.styles.section}>
-              <Text>Section #1</Text>
-            </View>
-            <View style={this.styles.section}>
-              <Text>Section #2</Text>
-            </View>
-            <Image src={src}></Image>
-          </Page>
-          <Page size="A4" style={this.styles.page}>
-            <View style={this.styles.section}>
-              <Text>Section #1</Text>
-            </View>
-            <View style={this.styles.section}>
-              <Text>Section #2</Text>
-            </View>
-          </Page>
-        </Document>
-    ))})));
+class FormSchema {
+  @extraSpace
+  Address = ""
+  proximity : calculated = (o =>  `Located at ${o.location ?? ""}`)
   
+  location? : LatLngExpression
+
+  @validate(str => `$${str}`)
+  salesPrice : number = 0;
+
+
+  @options(["Closed", "Active", "Pending"])
+  status = ""
+
+  floorHeight = 0;
+
+  @options([
+    "Water", 
+    "Wood",
+    "Park",
+    "Golf Course",
+    "City/Skyline",
+    "Mountain",
+    "Residential",
+    "City Street",
+    "Industrial",
+    "Power Lines",
+    "Limited Sight",
+    "Other"])
+  view = "";
+
+  @options([
+    "Complete disaster",
+    "Original but needs work",
+    "Original but well kept",
+    "Some updates/renovation",
+    "Substantial remodel",
+    "Rebuilt as new or new construction"
+  ])
+  condition = "";
+
+  bedrooms = 0;
+  halfBaths = 0;
+  fullBaths = 0;
+  livingAreaUnderAir = 0;
+
+  @options([
+    "Yes",
+    "No",
+    "Partial",
+  ])
+  basement = "";
+
+  @placeHolder("Garage/Carport")
+  @options([
+    "No Car Storage",
+    "Attached Garage",
+    "Detached Garage",
+    "Built-in Garage",
+    "Carport",
+    "Driveway",
+  ])
+  garage = "";
+
+  @options([
+    "Pool",
+    "Patio",
+    "Porch",
+    "Deck",
+  ])
+  @placeHolder("Pool/Patio/Porch/Deck")
+  pool = "";
+
+  @extraSpace
+  extraInfo = "";
+}
+
+type AppState = {};
+
+
+
+
+export default class App extends React.Component<{}, AppState> {
+
+
+
+  async geocode(address: string){
+      return fetch(encodeURI(`https://api.tomtom.com/search/2/geocode/${address}.json?key=${mapKey}`))
+      .then(response => response.json())
+      .then(response => response?.results ?? [{}])
+      .then(results => results[0]?.position).then(
+        (position?  :  {lat : number, lon: number}) => {
+          if(position){
+            let locationExpression = [position.lat, position.lon] as LatLngExpression
+            return locationExpression
+          }
+        }
+      )
   }
 
+  getComps(){
+    return this.comparableRefs.map(ref => ref?.current?.state?.location).filter(c => c) as LatLngExpression[];
+  }
+
+  constructor(props: {} | Readonly<{}>){
+    super(props);
+    this.geocode = this.geocode.bind(this);
+  }
+
+
+  comparableRefs = _.times(3, () => createRef<ObjectForm<FormSchema>>());
 
 
   render() {
-      return(
-      <div className = "App" >
+    return (
+      <div className="App">
         <div>
-          {this.myMap()}
+          count: {this.comparableRefs.length}
+          <Container fluid>
+          <Row>
+            {this.comparableRefs.map(c => (
+              <Col className="p-0" xs="12" lg="4">
+                <Button className="w-100" disabled>Comparable 1</Button>
+                <ObjectForm ref={c} defaultValues = {new FormSchema()} onLoseFocus = {{Address: ((address, form) => {
+                  this.geocode(address).then(p => {
+                    if(p != form.state.location){
+                      form.setState({location: p})
+                      this.forceUpdate()
+                    }
+                  })
+                })}}></ObjectForm>
+              </Col>
+            ))}
+          </Row>
+          </Container>
+          <Container className="justify-content-center">
+            {this.getComps().length ? <LocationMap positions = {this.getComps()}></LocationMap> : ""}
+          </Container>
         </div>
-        <button onClick={() => this.generatePdf(this.map)}>Click me!</button>
       </div>
     );
   }
